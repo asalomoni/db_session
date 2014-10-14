24deb143c5b03216256a2021266c02bb11579ea7
Andrea Salomoni <andrea.salomoni@intersail.it>
2014-10-14 10:08:23 +0200

    Change generator namespace

--- lib/generators/db_session/install_generator.rb
+++ lib/generators/db_session/install_generator.rb
@@ -1,22 +1,18 @@
-require 'rails/generators/active_record'
-
 module DbSession
   module Generators
-    class InstallGenerator < ActiveRecord::Generators::Base
-      #include Rails::Generators::Migration
-      #include ActiveRecord::Generators::Base
+    class InstallGenerator < Rails::Generators::Base
+      include Rails::Generators::Migration
 
       source_root File.expand_path("../templates", __FILE__)
 
       def copy_db_session_migration
-        migration_template 'create_db_session_stores.rb', 'db/migrate/create_db_session_stores.rb' unless migration_exists?('db/migrate/', 'create_db_session_stores.rb')
+        puts '>>>>>>>>>>>>>> ' if migration_exists?('db/migrate', 'create_db_session_stores.rb')
+        migration_template 'create_db_session_stores.rb', 'db/migrate/create_db_session_stores.rb' unless migration_exists?('db/migrate', 'create_db_session_stores.rb')
       end
 
-=begin
       def self.next_migration_number(dir)
-        Time.now.utc.strftime('%Y%m%d%H%M%S')
+        ActiveRecord::Generators::Migration.next_migration_number(dir)
       end
-=end
     end
   end
 end
