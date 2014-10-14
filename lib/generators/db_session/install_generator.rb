0d063871ec99dfab454c69388f783fd75871116f
Andrea Salomoni <andrea.salomoni@intersail.it>
2014-10-14 09:43:01 +0200

    Change generator namespace

--- lib/generators/db_session/install_generator.rb
+++ lib/generators/db_session/install_generator.rb
@@ -1,8 +1,8 @@
-require 'rails/generators/migration'
-
 module DbSession
   module Generators
     class InstallGenerator < Rails::Generators::Base
+      include Rails::Generators::Migration
+
       source_root File.expand_path("../templates", __FILE__)
 
       def copy_db_session_migration
