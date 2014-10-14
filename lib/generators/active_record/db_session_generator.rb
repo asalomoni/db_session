337fd2eff6ff9bfbee812437b7d47549f49ac8d8
Andrea Salomoni <andrea.salomoni@intersail.it>
2014-10-14 09:23:18 +0200

    Gem refactory

---
+++ lib/generators/active_record/db_session_generator.rb
@@ -0,0 +1,13 @@
+module ActiveRecord
+  module Generators
+    class DbSessionGenerator < Rails::Generators::Base
+      source_root File.expand_path("../templates", __FILE__)
+
+      def copy_db_session_migration
+        migration_template 'create_db_session_stores.rb', 'db/migrate/create_db_session_stores.rb'
+      end
+    end
+  end
+end
+
+
--- lib/tasks/db_session_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :db_session do
-#   # Task goes here
-# end
