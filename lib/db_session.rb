a8d0897e2a0285ddcf6056807b903ee69dd9396c
Andrea Salomoni <andrea.salomoni@intersail.it>
2014-10-16 12:44:41 +0200

    README change

--- lib/db_session.rb
+++ lib/db_session.rb
@@ -21,43 +21,58 @@ module DbSession
       main_data_object = JSON.parse(db_session.serialized_data)
 
       if key
-        return main_data_object[key.to_s]
+        return rebuild_object(main_data_object[key.to_s])
       else
+        main_data_object.each do |k, v|
+          main_data_object[k] = rebuild_object(main_data_object[k])
+        end
         return main_data_object
       end
     end
   end
 
-  def set_db_session(key, value)
+  def set_db_session(key, object)
     clear_db_session
 
-    main_data_object = {}
-    main_data_object[key] = value
+    can_be_stored = can_be_stored?(object)
 
-    db_session = DbSessionStore.create(serialized_data: main_data_object.to_json)
+    if can_be_stored
+      main_data_object = {}
+      main_data_object[key] = {class: get_class(object), object: object}
 
-    session[SESSION_KEY] = db_session.id
-
-    clear_expired_sessions
-  end
-
-  def add_to_db_session(key, value)
-    db_session_id = session[SESSION_KEY]
-    db_session_id ? db_session = DbSessionStore.find_by(id: db_session_id) : db_session = nil
-
-    db_session ? main_data_object = JSON.parse(db_session.serialized_data) : main_data_object = {}
-    main_data_object[key] = value
-
-    if db_session
-      db_session.serialized_data = main_data_object.to_json
-      db_session.save
-    else
       db_session = DbSessionStore.create(serialized_data: main_data_object.to_json)
+
+      session[SESSION_KEY] = db_session.id
     end
 
-    session[SESSION_KEY] = db_session.id
+    clear_expired_sessions
+
+    can_be_stored
+  end
+
+  def add_to_db_session(key, object)
+    can_be_stored = can_be_stored?(object)
+
+    if can_be_stored
+      db_session_id = session[SESSION_KEY]
+      db_session_id ? db_session = DbSessionStore.find_by(id: db_session_id) : db_session = nil
+
+      db_session ? main_data_object = JSON.parse(db_session.serialized_data) : main_data_object = {}
+      main_data_object[key] = {class: get_class(object), object: object}
+
+      if db_session
+        db_session.serialized_data = main_data_object.to_json
+        db_session.save
+      else
+        db_session = DbSessionStore.create(serialized_data: main_data_object.to_json)
+      end
+
+      session[SESSION_KEY] = db_session.id
+    end
 
     clear_expired_sessions
+
+    can_be_stored
   end
 
   def clear_db_session
@@ -69,6 +84,37 @@ module DbSession
   end
 
   private
+
+  def rebuild_object(obj)
+    if obj[:class]
+      class_from_string(obj[:class]).new.assign_attributes(obj[:object])
+    else
+      obj[:object]
+    end
+  end
+
+  def get_class(obj)
+    if obj
+      if obj.class.ancestors.include?(ActiveRecord::Base) && obj.respond_to?(:assign_attributes)
+        obj.class.name
+      end
+    end
+  end
+
+  def can_be_stored?(obj)
+    begin
+      JSON.parse(obj.to_json)
+      return true
+    rescue
+      return false
+    end
+  end
+
+  def class_from_string(str)
+    str.split('::').inject(Object) do |mod, class_name|
+      mod.const_get(class_name)
+    end
+  end
 
   def clear_expired_sessions
     begin
